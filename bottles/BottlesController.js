39624aceaedd4377560b6bd36d00eabc0d101586
Aldo Donisete Fernandes Júnior <aldo.dfernandes@gmail.com>
2018-03-14 16:07:10 -0300

    Criação de uma rota que não precisa de autenticação, para retornar as informações dos bottles

--- bottles/BottlesController.js
+++ bottles/BottlesController.js
@@ -73,4 +73,54 @@ router.get('/infos', VerifyToken, function (req, res) {
   res.json(infos);
 });
 
+router.get('/getinfos', function (req, res) {
+  var infos = [
+    {
+      "bottle": "1",
+      "type":"Pilsner",
+      "minimum_temp":"-6",
+      "maximum_temp":"-4",
+      "current_temp":getRandomInt(-7,-3)
+    },
+    {
+      "bottle": "2",
+      "type":"IPA",
+      "minimum_temp":"-6",
+      "maximum_temp":"-5",
+      "current_temp":getRandomInt(-7,-4)
+    },
+    {
+      "bottle": "3",
+      "type":"Lager",
+      "minimum_temp":"-7",
+      "maximum_temp":"-4",
+      "current_temp":getRandomInt(-8,-3)
+    },
+    {
+      "bottle": "4",
+      "type":"Stout",
+      "minimum_temp":"-8",
+      "maximum_temp":"-6",
+      "current_temp":getRandomInt(-9,-5)
+    },
+    {
+      "bottle": "5",
+      "type":"Wheat beer",
+      "minimum_temp":"-5",
+      "maximum_temp":"-3",
+      "current_temp":getRandomInt(-6,-2)
+    },
+    {
+      "bottle": "6",
+      "type":"Pale Ale",
+      "minimum_temp":"-6",
+      "maximum_temp":"-4",
+      "current_temp":getRandomInt(-7,-3)
+    }
+  ];
+  res.header("Access-Control-Allow-Origin", "*");
+  res;header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+  res.json(infos);
+});
+
 module.exports = router;
